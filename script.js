187a57453a5330a3d9ce5bdd428c40e7bebae6bc
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-11-23 18:42:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function daysOfAYear(y) {
+
+	if((y%4==0&&y%100!=0)||(y%400==0))
+		return 366;
+	return 365;  
+}
+
+let x=prompt();
+alert(daysOfAYear(x));
